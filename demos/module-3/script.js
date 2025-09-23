94114ac403d4b49f03df7e98bb19937df5c82c17
Allendale Nato <anato2@academic.rrc.ca>
2025-09-23 15:17:57 -0500

    feat: Explore comments and variable types

---
+++ demos/module-3/script.js
@@ -0,0 +1,15 @@
+/*
+  This is a multi-line comment.
+  Name: Allendale Nato
+  Date: September 23, 2025
+  Info: This is an introduction to JavsScript.
+*/
+
+// Single Line Comment
+
+// Variables
+const neverChange = 404; // Constant number.
+let welcomeMessage = "Hello, who might you be?"; // Mutable string
+var legacyVariable = "I am from olden times"; // Deprecated
+
+// Data Types
