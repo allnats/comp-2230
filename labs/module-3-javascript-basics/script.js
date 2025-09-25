0665045534803a9aa950990c285803dbb2bc1ceb
Allendale Nato <anato2@academic.rrc.ca>
2025-09-25 09:28:15 -0500

    feat: update header to dynamically display planet name

--- labs/module-3-javascript-basics/script.js
+++ labs/module-3-javascript-basics/script.js
@@ -1,3 +1,19 @@
+/*
+    Planet details.
+    Chosen Planet: Pluto.
+
+    Defined three different variable keywords.
+*/
+
 const planetName = "Pluto";
 let discoveryYear = "February 18, 1930";
 var planetType = "Dwarf planet";
+
+function populateHeader() {
+    const header = document.getElementById("celestial-name");
+    header.textContent = planetName;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    populateHeader();
+});
