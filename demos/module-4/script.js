fdb1c36318f993ef11c663465cd3ad034ff1541f
Allendale Nato <anato2@academic.rrc.ca>
2025-10-07 15:00:22 -0500

    chore: add module 4 demo starter files

---
+++ demos/module-4/script.js
@@ -0,0 +1,16 @@
+/*
+    Javascript Forms Demo
+
+
+*/
+
+
+//  Where the heavy lifting for validation is done
+const validateForm = () => {
+
+    //const simpleEmailPattern = /.+@.+\..+/;
+
+    // A more complex regex pattern for email validation
+    //const complexEmailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+}
