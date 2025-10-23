f350cbfd4796ecdc3ddddf889c28483649140ae0
Allendale Nato <anato2@academic.rrc.ca>
2025-10-23 09:33:10 -0500

    feat: fetch and print cat breeds to console.

--- labs/module-5-async/script.js
+++ labs/module-5-async/script.js
@@ -15,3 +15,18 @@ async function demoAsyncCall() {
 }
 
 demoAsyncCall();
+
+async function fetchCatBreeds() {
+    const reqUrl = "https://catfact.ninja/breeds";
+    try {
+        const res = await fetch(reqUrl);
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        throw new Error("Error fetching Cat Breeds!");
+    }
+}
+
+fetchCatBreeds()
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
