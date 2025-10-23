6e857670cc7a36981196f279003475fbe4c4e7ba
Allendale Nato <anato2@academic.rrc.ca>
2025-10-23 09:25:58 -0500

    feat: add async simulation in lab 5

--- labs/module-5-async/script.js
+++ labs/module-5-async/script.js
@@ -1,1 +1,17 @@
 function displayCatFacts(facts) {}
+
+function simulateAsyncOperation() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("simulateAsyncOperation: 5s has elapsed!");
+        }, 5000);
+    });
+}
+
+async function demoAsyncCall() {
+    console.log("Starting async simulation...");
+    const result = await simulateAsyncOperation();
+    console.log(result);
+}
+
+demoAsyncCall();
